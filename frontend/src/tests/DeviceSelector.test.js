6e2d99204112b597ff2963131bbe21bf5e17805f
Roman Chubugin <kinnegan@gmail.com>
2024-12-11 22:45:28 +0300

    fix lint

--- frontend/src/tests/DeviceSelector.test.js
+++ frontend/src/tests/DeviceSelector.test.js
@@ -5,7 +5,7 @@ import DeviceSelector from "../components/DeviceSelector";
 test("renders device selector", () => {
   const devices = [
     { _id: "1", external_id: "device1", dev_type: "Type 1" },
-    { _id: "2", external_id: "device2", dev_type: "Type 2" },
+    { _id: "2", external_id: "device2", dev_type: "Type 2" }
   ];
   const onSelect = jest.fn();
 
